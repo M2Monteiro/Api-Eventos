be69e116ae71b8d2817bbd25f725929f0ceaa396
Matheus <matheus.stack.20@hotmail.com>
2024-09-11 14:29:21 -0300

    Criando os Models

--- eventin/models.py
+++ eventin/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    local = models.CharField(max_length=100)
+    data_evento = models.DateTimeField()
+    capacidade = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.titulo
+
+
+class Paticipante(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11)
+    email = models.EmailField(max_length=100)
+    telefone = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
