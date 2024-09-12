af12845281260d9bba31872cc4f5a7f77617a8ef
Matheus <matheus.stack.20@hotmail.com>
2024-09-12 09:22:53 -0300

    Configurando modelos no admin

--- eventin/admin.py
+++ eventin/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Evento, Paticipante
 
-# Register your models here.
+
+class Eventos(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "titulo",
+        "local",
+        "data_evento",
+        "capacidade",
+    )
+    list_display_links = (
+        "id",
+        "titulo",
+    )
+    list_per_page = 20
+    search_fields = (
+        "titulo",
+        "local",
+    )
+
+
+admin.site.register(Evento, Eventos)
+
+
+class Participantes(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "nome",
+        "cpf",
+        "email",
+        "telefone",
+    )
+    list_display_links = (
+        "id",
+        "nome",
+    )
+    list_per_page = 20
+    search_fields = (
+        "nome",
+        "email",
+    )
+
+admin.site.register(Paticipante, Participantes)
